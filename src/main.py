03f099e0a6d53a566deed533cd6ceb0f1a0cb301
credo7 <ah.vit@mail.ru>
2024-03-02 14:38:04 +0300

    fast fix

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api import auth, group, user, event, ws
+from api import auth, group, user, event, ws, dev
 from services.error_log_handling_middleware import ErrorLogHandlingMiddleware
 from tg_logger import tg_wrapper
 
@@ -27,6 +27,7 @@ app.include_router(auth.router)
 app.include_router(group.router)
 app.include_router(user.router)
 app.include_router(event.router)
+app.include_router(dev.router)
 # app.include_router(lesson.router)
 app.include_router(ws.router)
 
