b79cdd9e150ab5c3f906279c19a2f83d776d0953
credo7 <ah.vit@mail.ru>
2024-12-20 09:58:43 +0300

    update

--- src/services/practice_two_class.py
+++ src/services/practice_two_class.py
@@ -649,9 +649,11 @@ class PracticeTwoClass:
 
             checkpoint_response.hint = f'заполняем risk_codes_desc'
 
-            event.risks_chosen_by_user[0] = RisksWithRouteName(
-                route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
-                risks=[risks_mapping[id] for id in checkpoint_dto.risk_ids_desc],
+            event.risks_chosen_by_user.append(
+                RisksWithRouteName(
+                    route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
+                    risks=[risks_mapping[id] for id in checkpoint_dto.risk_ids_desc],
+                )
             )
 
         if checkpoint_dto.step_code == 'SCREEN_10_RISKS_2':
@@ -663,9 +665,11 @@ class PracticeTwoClass:
 
             risks_mapping = {risk.id: risk for risk in pr2_class_info.all_risks[1]}
 
-            event.risks_chosen_by_user[1] = RisksWithRouteName(
-                route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
-                risks=[risks_mapping[id] for id in checkpoint_dto.risk_codes_desc],
+            event.risks_chosen_by_user.append(
+                RisksWithRouteName(
+                    route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
+                    risks=[risks_mapping[id] for id in checkpoint_dto.risk_ids_desc],
+                )
             )
 
         if checkpoint_dto.step_code == 'SCREEN_10_RISKS_3':
@@ -677,9 +681,11 @@ class PracticeTwoClass:
 
             risks_mapping = {risk.id: risk for risk in pr2_class_info.all_risks[2]}
 
-            event.risks_chosen_by_user[2] = RisksWithRouteName(
-                route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
-                risks=[risks_mapping[id] for id in checkpoint_dto.risk_codes_desc],
+            event.risks_chosen_by_user.append(
+                RisksWithRouteName(
+                    route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
+                    risks=[risks_mapping[id] for id in checkpoint_dto.risk_ids_desc],
+                )
             )
 
         if checkpoint_dto.step_code == 'SCREEN_10_RISKS_4':
@@ -691,9 +697,11 @@ class PracticeTwoClass:
 
             risks_mapping = {risk.id: risk for risk in pr2_class_info.all_risks[3]}
 
-            event.risks_chosen_by_user[3] = RisksWithRouteName(
-                route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
-                risks=[risks_mapping[id] for id in checkpoint_dto.risk_codes_desc],
+            event.risks_chosen_by_user.append(
+                RisksWithRouteName(
+                    route_name='МАРШРУТ ЧЕРЕЗ РОССИЮ (ЧЕРЕЗ ЗАБАЙКАЛЬСК И БРЕСТ)',
+                    risks=[risks_mapping[id] for id in checkpoint_dto.risk_ids_desc],
+                )
             )
 
         if checkpoint_dto.step_code == 'SCREEN_10_RISKS_TOTAL':
